e14886d5ad5c6f86a35ac14e009c3944b6117d57
Ember Z <417002107@qq.com>
2022-02-16 09:48:48 +0800

    增加 /order 命令，查询最近7天的返利订单

--- fanli-bot_ember/src/app.ts
+++ fanli-bot_ember/src/app.ts
@@ -6,14 +6,15 @@ const HttpsProxyAgent = require('https-proxy-agent');
 const BOT_TOKEN = '';//你申请到的bot token
 const UNION_ID = '';//抓取你自己的京东联盟ID
 const config = {
-    url: 'http://127.0.0.1:5700',//你的青龙地址:端口
-    clientID: '', //你的应用ID
-    clientSecret: '', //你的应用Secret
+    url: 'http://127.0.0.1:5700',//青龙地址:端口
+    clientID: '',
+    clientSecret: '',
     searchValue: 'JD_COOKIE',
     index: 1, //默认获取第一个 cookie
-    agentHost: '127.0.0.1',//代理地址,没有就置空
-    agentPort: 7890  //代理端口，,没有就置空
+    agentHost: '127.0.0.1',
+    agentPort: 7890
 };
+
 let bot;
 if (config.agentHost && config.agentPort) {
     const agent = new HttpsProxyAgent({
@@ -34,12 +35,45 @@ bot.launch().then(r => {
  */
 bot.hears(/\/start/, ctx => {
     try {
-        ctx.reply('京东转链，请回复商品 url 来获取返利信息，例如直接发送 https://item.jd.com/70241615154.html');
+        ctx.reply('京东转链机器人'+'\n1.请回复商品 url 来获取返利信息，例如直接发送 https://item.jd.com/70241615154.html' +
+            '\n 2.回复 /order 查询最近7天返利订单');
     } catch (error) {
         console.log('/start', error);
     }
 });
 
+/**
+ * 监听查询最近7天订单请求
+ */
+bot.hears(/\/order/, ctx => {
+    try {
+        getOrder().then(res => {
+            let linkInfo = res.data;
+            if (linkInfo && linkInfo.code === 200) {//如果转链接结果是否存在
+                let data = linkInfo.result;
+                if (data.length === 0) {
+                    ctx.reply('没有订单哦！');
+                } else {
+                    for (let i = 0; i < data.length; i++) {
+                        let replyMsg = data[i].skuName + '\n' + '----------------' + '\n' + `预计佣金:¥${data[i].estimateFee}`;
+                        ctx.replyWithPhoto({url: `http://img14.360buyimg.com/n1/${data[i].skuImgUrl}`},
+                            {caption: replyMsg});
+                    }
+                }
+
+            } else {//转链失败
+                ctx.reply('查询失败了');
+            }
+        });
+
+    } catch (error) {//捕获错误
+        console.log('转链', error);//记录错误
+        //尝试给用户回复消息安抚
+        ctx.reply('发生了无法预料的错误，请检查程序。').catch(err => {
+            console.log(err);
+        });
+    }
+});
 /**
  * 监听转链请求
  */
@@ -80,7 +114,6 @@ bot.hears(/^(?:(http|https|ftp):\/\/)?((|[\w-]+\.)+[a-z0-9]+)(?:(\/[^/?#]+)*)?(\
         });
     }
 });
-
 
 async function getCookie() {
     const url = `${config.url}/open/auth/token?client_id=${config.clientID}&client_secret=${config.clientSecret}`;
@@ -129,3 +162,74 @@ async function converUrl(url: string) {
 
 
 }
+
+
+async function getOrder() {
+    // 实例化一个URL对象
+    let reqLink = new URL('https://api.m.jd.com/api');
+    // 预先准备好请求参数
+    let startTime = new Date().getTime() - 7 * 24 * 60 * 60 * 1000;
+
+
+    let payload = {
+        functionId: 'listOrderSku',
+        appid: 'u',
+        _: Date.now(),
+        body: JSON.stringify({
+            funName: 'listOrderSku',
+            param: {
+                // @ts-ignore
+                startTime: new Date(startTime).format('yyyy-MM-dd hh:mm:ss'),
+                // @ts-ignore
+                endTime: new Date().format('yyyy-MM-dd hh:mm:ss'),
+                orderStatus: 0,
+                optType: 1,
+            },
+            page: {
+                pageNo: 1,
+                pageSize: 10
+            },
+            unionId: UNION_ID,
+        }),
+        loginType: 2
+    };
+    // 通过循环依次将请求参数添加到URL对象中
+    for (const vo of Object.keys(payload)) {
+        reqLink.searchParams.append(vo, payload[vo]);
+    }
+    // 发起请求
+    const Cookie = await getCookie();
+
+    return axios.get(reqLink.href, {
+        headers: {
+            Cookie,
+            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x1800122b) NetType/WIFI Language/zh_CN'
+        }
+    });
+
+
+}
+
+// @ts-ignore
+Date.prototype.format = function (fmt) {
+    var o = {
+        'M+': this.getMonth() + 1,                 //月份
+        'd+': this.getDate(),                    //日
+        'h+': this.getHours(),                   //小时
+        'm+': this.getMinutes(),                 //分
+        's+': this.getSeconds(),                 //秒
+        'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+        'S': this.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in o) {
+        if (new RegExp('(' + k + ')').test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+        }
+    }
+    return fmt;
+};
+
+
